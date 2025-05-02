88100533e28946649561bac4ec6309e42a05b6e3
AlexPaturov <alexey.paturov@gmail.com>
2025-05-02 12:45:56 +0300

    Add project files.

---
+++ InputFields/Program.cs
@@ -0,0 +1,21 @@
+using InputFields.Components;
+
+namespace InputFields
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+            var app = builder.Build();
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+            app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+            app.Run();
+        }
+    }
+}
